62a61293da976109534244cb25e42e260a1223a1
Anthony Lannoote <atl@melexis.com>
2023-12-04 08:29:47 +0100

    add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Header } from '@/components/Header/Header';
+import { Hero } from '@/components/Hero/Hero';
+import { CardsCarousel } from '@/components/Highlights/HighLights';
+import { Footer } from '@/components/Footer/Footer';
+import { Features } from '@/components/Features/Features';
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
+    <Header/>
+      <Hero />
+      <Features/>
+      <CardsCarousel/>
+      <Footer/>
       <ColorSchemeToggle />
     </>
   );
